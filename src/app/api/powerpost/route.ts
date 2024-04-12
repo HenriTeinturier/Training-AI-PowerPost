36060a2cb21534a0ac0a7363b89964b1310f8338
Teinturier <henri.teinturier@gmail.com>
2024-04-12 18:51:21 +0200

    save powerpost to database

--- src/app/api/powerpost/route.ts
+++ src/app/api/powerpost/route.ts
@@ -3,22 +3,55 @@ import { NextResponse } from "next/server";
 import { scrapPost } from "./scrap-post";
 import { generatePowerpost } from "./generate-powerpost";
 import { generateTitle } from "./generate-title";
+import { auth, requiredAuth } from "@/auth/helper";
+import { prisma } from "@/prisma";
 
 export const POST = async (req: Request) => {
-  const body = await req.json();
-  const data = PostSchema.parse(body);
+  const user = await requiredAuth();
 
-  const { markdown, coverUrl } = await scrapPost(data.source);
+  if (!user) {
+    return NextResponse.json(
+      { error: "You must be authenticated to access this resource." },
+      { status: 401 }
+    );
+  }
 
-  const powerpost = await generatePowerpost({
-    markdown,
-    mode: data.mode,
-    language: data.language,
-  });
+  try {
+    const body = await req.json();
+    const data = PostSchema.parse(body);
 
-  const title = await generateTitle({
-    markdown,
-  });
+    const { markdown, coverUrl } = await scrapPost(data.source);
 
-  return NextResponse.json(data);
+    const powerpost = await generatePowerpost({
+      markdown,
+      mode: data.mode,
+      language: data.language,
+    });
+
+    const title = await generateTitle({
+      markdown,
+    });
+
+    const finalPost = await prisma.post.create({
+      data: {
+        title,
+        content: markdown,
+        powerPost: markdown,
+        source: data.source,
+        coverUrl,
+        id:
+          title.replaceAll(" ", "-").toLowerCase() +
+          "-" +
+          Math.random().toString(36).substring(7),
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json(finalPost);
+  } catch (e) {
+    if (e instanceof Error) {
+      return NextResponse.json({ error: e.message }, { status: 400 });
+    }
+    return NextResponse.json({ error: "An error occurred" }, { status: 400 });
+  }
 };
--- src/app/dashboard/posts/new/PostForm.tsx
+++ src/app/dashboard/posts/new/PostForm.tsx
@@ -58,86 +58,97 @@ const PostForm = (props: PostFormProps) => {
   });
 
   return (
-    <Form {...form} disabled={postMutation.isPending}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-4"
-      >
-        <FormField
-          control={form.control}
-          name="source"
-          render={({ field }) => (
-            <FormItem className="">
-              <FormLabel>Source</FormLabel>
-              <FormControl>
-                <Input placeholder="https://medium.com/..." {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="mode"
-          render={({ field }) => (
-            <FormItem className="flex flex-col gap-2 border border-border p-2">
-              <FormLabel>Mode</FormLabel>
-              <FormControl>
-                <ToggleGroup
-                  className="flex flex-wrap gap-2"
-                  type="single"
-                  defaultValue={field.value}
-                  onValueChange={field.onChange}
-                >
-                  {Object.entries(PostMode).map(([key, value]) => {
-                    const data = PostModeDataMap[key as PostMode];
-                    return (
-                      <ToggleGroupItem
-                        key={key}
-                        value={key}
-                        className="flex flex-col gap-2 h-auto p-4"
-                      >
-                        {<data.icon size={24} />}
-                        {<span>{value}</span>}
-                      </ToggleGroupItem>
-                    );
-                  })}
-                </ToggleGroup>
-              </FormControl>
-
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="language"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Language</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="source"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel>Source</FormLabel>
                 <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Select a language" />
-                  </SelectTrigger>
+                  <Input
+                    placeholder="https://medium.com/..."
+                    {...field}
+                    disabled={postMutation.isPending}
+                  />
                 </FormControl>
-                <SelectContent>
-                  {Object.entries(LANGUAGES).map(([key, value]) => (
-                    <SelectItem key={key} value={key}>
-                      {value} {key}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">
-          {postMutation.isPending ? "Creating ..." : "Create"}
-        </Button>
-      </form>
-    </Form>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="mode"
+            render={({ field }) => (
+              <FormItem className="flex flex-col gap-2 border border-border p-2">
+                <FormLabel>Mode</FormLabel>
+                <FormControl>
+                  <ToggleGroup
+                    className="flex flex-wrap gap-2"
+                    type="single"
+                    defaultValue={field.value}
+                    onValueChange={field.onChange}
+                  >
+                    {Object.entries(PostMode).map(([key, value]) => {
+                      const data = PostModeDataMap[key as PostMode];
+                      return (
+                        <ToggleGroupItem
+                          key={key}
+                          value={key}
+                          className="flex flex-col gap-2 h-auto p-4"
+                          disabled={postMutation.isPending}
+                        >
+                          {<data.icon size={24} />}
+                          {<span>{value}</span>}
+                        </ToggleGroupItem>
+                      );
+                    })}
+                  </ToggleGroup>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="language"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Language</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  disabled={postMutation.isPending}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select a language" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {Object.entries(LANGUAGES).map(([key, value]) => (
+                      <SelectItem key={key} value={key}>
+                        {value} {key}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={postMutation.isPending}>
+            {postMutation.isPending ? "Creating ..." : "Create"}
+          </Button>
+        </form>
+      </Form>
+    </>
   );
 };
 
