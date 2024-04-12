e5e15a6a5b8996a6ba90dad612b0aedf2ebd924b
Teinturier <henri.teinturier@gmail.com>
2024-04-12 16:06:39 +0200

    scrap post

--- src/app/api/powerpost/route.ts
+++ src/app/api/powerpost/route.ts
@@ -1,9 +1,15 @@
 import { PostSchema } from "@/app/dashboard/posts/new/post.schema";
 import { NextResponse } from "next/server";
+import { scrapPost } from "./scrap-post";
 
 export const POST = async (req: Request) => {
   const body = await req.json();
   const data = PostSchema.parse(body);
+
+  const { markdown, coverUrl } = await scrapPost(data.source);
+
+  console.log("powerpost", markdown);
+  console.log("cover url", coverUrl);
 
   return NextResponse.json(data);
 };
