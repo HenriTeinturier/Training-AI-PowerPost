5bbec9b7c095c1058f4677ebb758c3b9739b7872
Teinturier <henri.teinturier@gmail.com>
2024-04-16 18:57:12 +0200

    add landing page

--- src/components/features/layout/Header.tsx
+++ src/components/features/layout/Header.tsx
@@ -4,19 +4,21 @@ import { ThemeToggle } from "./ThemeToggle";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="mt-0 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <div className="flex items-center gap-2">
-          <Link href="/" className="text-xl font-bold">
-            PowerPost
-          </Link>
-        </div>
+    <header className="sticky flex  top-0 z-40 w-full border-b bg-background justify-center">
+      <div className="flex w-full max-w-screen-xl justify-between ">
+        <div className="mt-0 w-full flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+          <div className="flex items-center gap-2 m-2 ">
+            <Link href="/" className="text-xl font-bold">
+              PowerPost
+            </Link>
+          </div>
 
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <AuthButton />
-            <ThemeToggle />
-          </nav>
+          <div className="flex flex-1 items-center justify-end space-x-4">
+            <nav className="flex items-center space-x-1">
+              <AuthButton />
+              <ThemeToggle />
+            </nav>
+          </div>
         </div>
       </div>
     </header>
