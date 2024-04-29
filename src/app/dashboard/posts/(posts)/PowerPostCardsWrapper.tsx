29703e19c1f1880d882de5ee7f59c9157e487254
Teinturier <henri.teinturier@gmail.com>
2024-04-29 17:03:13 +0200

    display pagination when change page

--- src/app/dashboard/posts/(posts)/PowerPostCardsWrapper.tsx
+++ src/app/dashboard/posts/(posts)/PowerPostCardsWrapper.tsx
@@ -4,6 +4,8 @@ import PowerpostCards from "./PowerPostCards";
 import { getPosts } from "@/data/datasFunction";
 import { useQuery } from "@tanstack/react-query";
 import { Post } from "@prisma/client";
+import { useEffect, useState } from "react";
+import PostPagination from "./pagination";
 
 const PowerPostCardsWrapper = ({
   searchParams,
@@ -15,23 +17,37 @@ const PowerPostCardsWrapper = ({
     sort?: string;
   };
 }) => {
-  const { data } = useQuery<{ posts: Post[]; count: number }>({
+  const [totalPage, setTotalPage] = useState<null | number>(1);
+
+  const { data, isSuccess } = useQuery<{ posts: Post[]; count: number }>({
     queryKey: ["posts", searchParams],
     queryFn: () => getPosts(searchParams),
+    // {
+    //   onSuccess: (test) => {
+    //     setTotalPage(test.count);
+    //   },
+    // }
   });
 
-  return data ? (
+  useEffect(() => {
+    if (isSuccess) {
+      setTotalPage(data?.count);
+    }
+  }, [isSuccess, data]);
+
+  return (
     <div className="flex flex-wrap gap-4 justify-center">
       <div className="flex flex-wrap gap-4 justify-center">
-        <PowerpostCards posts={data.posts} totalPage={data.count} />
-      </div>
-    </div>
-  ) : (
-    <div className="flex flex-wrap gap-4 justify-center">
-      <div className="flex flex-wrap gap-4 justify-center">
-        {Array.from({ length: 4 }).map((_, index) => (
-          <PowerPostCardsSkeleton key={index} />
-        ))}
+        {data ? (
+          <PowerpostCards posts={data.posts} totalPage={data.count} />
+        ) : (
+          <>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <PowerPostCardsSkeleton key={index} />
+            ))}
+          </>
+        )}
+        {totalPage && <PostPagination totalPages={totalPage} />}
       </div>
     </div>
   );
