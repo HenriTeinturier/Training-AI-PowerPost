2f09282754ed1ce9ba9d387a6b8fc16af184ace2
Teinturier <henri.teinturier@gmail.com>
2024-05-02 12:57:56 +0200

    fix darklogo after refresh

--- src/components/features/layout/CreateLogo.tsx
+++ src/components/features/layout/CreateLogo.tsx
@@ -3,35 +3,26 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import powerpostLogoSvg from "../../../../public/assets/logos/powerpost-logo.svg";
 import powerpostLogoSvgDark from "../../../../public/assets/logos/powerpost-logo-dark.svg";
+import { useEffect, useState } from "react";
 
 const CreateLogo = () => {
-  const defaultTheme =
-    typeof window !== "undefined"
-      ? localStorage.getItem("theme") || "light"
-      : "light";
-  const { setTheme, theme } = useTheme();
+  const { theme, resolvedTheme } = useTheme();
+  const [logoSrc, setLogoSrc] = useState(powerpostLogoSvg);
 
-  const LogoToDisplay = (): string => {
-    let trueTheme = defaultTheme;
-    if (theme) {
-      trueTheme = theme;
-    }
-
-    if (trueTheme === "dark") {
-      return powerpostLogoSvgDark;
+  useEffect(() => {
+    if (theme === "dark" || resolvedTheme === "dark") {
+      setLogoSrc(powerpostLogoSvgDark);
     } else {
-      return powerpostLogoSvg;
+      setLogoSrc(powerpostLogoSvg);
     }
-  };
+  }, [theme, resolvedTheme]);
 
   return (
-    <div>
-      <div className="flex flex-row  items-start justify-start ">
-        <div className="h-6 w-4 block Slot">
-          <Image src={LogoToDisplay()} alt="PowerPost Logo" />
-        </div>
-        <div className="font-bold">owerPost</div>
+    <div className="flex flex-row  items-start justify-start ">
+      <div className="h-6 w-4 block Slot">
+        <Image src={logoSrc} alt="PowerPost Logo" />
       </div>
+      <div className="font-bold">owerPost</div>
     </div>
   );
 };
